0959d2bdbe7d7dd7ad494ad18e2455a8e57d1047
aleksandr.sychev <aleksandr.sychev@nordigy.ru>
2024-11-08 18:58:00 +0400

    env variables

--- src/config.js
+++ src/config.js
@@ -6,16 +6,16 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // upload folder config
-export const UPLOAD_DIR_NAME = 'uploads';
+export const UPLOAD_DIR_NAME = process.env.UPLOAD_DIR_NAME;
 export const UPLOAD_DIR_PATH = path.join(__dirname, '../' + UPLOAD_DIR_NAME);
 
 // request config
-export const IMAGE_FIELD = 'upload';
-export const UPLOAD_PATH = '/upload';
+export const IMAGE_FIELD = process.env.IMAGE_FIELD;
+export const UPLOAD_URL = process.env.UPLOAD_URL;
 
 // resize config
-export const IMAGE_REDUCED_WIDTH = 1024;
+export const IMAGE_REDUCED_WIDTH = Number(process.env.IMAGE_REDUCED_WIDTH);
 
 // server config
-export const PORT = 3333;
+export const PORT = process.env.PORT;
 export const URL = `http://localhost:${PORT}`;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import { getUploader } from './uploader.js';
 import { getFileSizeMB, resize } from './resizer.js';
 import { post, startServer, useCORS, useStatic } from './server.js';
-import { IMAGE_FIELD, PORT, UPLOAD_DIR_NAME, UPLOAD_PATH, UPLOAD_DIR_PATH, URL, IMAGE_REDUCED_WIDTH } from './config.js';
+import { IMAGE_FIELD, PORT, UPLOAD_DIR_NAME, UPLOAD_URL, UPLOAD_DIR_PATH, URL, IMAGE_REDUCED_WIDTH } from './config.js';
 
 useCORS();
 useStatic(UPLOAD_DIR_NAME, UPLOAD_DIR_PATH);
 
 const uploader = getUploader(UPLOAD_DIR_PATH, IMAGE_FIELD);
 
-post(UPLOAD_PATH, (req, res, next) => {
+post(UPLOAD_URL, (req, res, next) => {
   uploader(req, res, async () => {
     const {originalname, path, filename} = req.file;
     console.log(`File uploaded succesfully:`, originalname, '->', path);
